7ece668ea929721f96fb758d3a8134e21f569fba
huaisun <420007900@qq.com>
2019-04-07 20:01:33 +0800

    完成前后端分离，跨域配置，接口配置等，以及登陆服务完成

--- src/main/java/com/huaisun/graduation/controller/EmployController.java
+++ src/main/java/com/huaisun/graduation/controller/EmployController.java
@@ -7,10 +7,7 @@ import com.huaisun.graduation.util.Result;
 import com.huaisun.graduation.util.Tools;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -30,7 +27,7 @@ public class EmployController {
 
     @ApiOperation(value = "员工管理[员工登陆]")
     @PostMapping("/login")
-    public Result login(EmployForm form, HttpServletRequest request) {
+    public Result login(@RequestBody EmployForm form, HttpServletRequest request) {
         HttpSession session = request.getSession();
         Result result = service.searchEmploy(form);
         session.setAttribute("employ", result.getData());
@@ -53,9 +50,9 @@ public class EmployController {
         HttpSession session = request.getSession();
 
         Object object = session.getAttribute("employ");
-        if(Tools.isEmpty(object)){
+        if (Tools.isEmpty(object)) {
             result.setResultCode(ResultCode.EMPLOY_NOT_LOGGED_IN);
-        }else {
+        } else {
             result.setResultCode(ResultCode.SUCCESS);
             result.setData(object);
         }
