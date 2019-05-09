5c0e161044513d47954c0a8f93bda7fb1fde11c5
huaisun <420007900@qq.com>
2019-05-09 17:56:52 +0800

    第二版。。。

---
+++ src/main/java/com/huaisun/graduation/order/entity/DetailOrder.java
@@ -0,0 +1,19 @@
+package com.huaisun.graduation.order.entity;
+
+import lombok.Data;
+
+/**
+ * @author sunruiguang
+ * @date 2019/5/9
+ * Updated by sunruiguang on 2019/5/9.
+ */
+@Data
+public class DetailOrder {
+
+    private Integer mId;
+
+    private String milkName;
+
+    private Integer number;
+
+}
